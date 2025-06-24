3b520ceccf41c7482a30b5e2857f29ed0185d8d6
Mostafijur <mrkajol550@gmail.com>
2025-06-24 22:06:24 +0600

    Refactor sum function to use named return values and update showDefer to demonstrate changes

--- defer/defer.go
+++ defer/defer.go
@@ -8,7 +8,7 @@ func DeferExample() {
 }
 
 func showDefer() {
-	res := sum(10, 20)
+	res := sumNamedReturnedValueFunction(10, 20)
 	fmt.Println("Main Sum : ", res)
 
 	res2 := defWithoutNamed(10, 20)
@@ -16,7 +16,12 @@ func showDefer() {
 }
 
 // this way of writing a function is called named return value.
-func sum(a, b int) (result int) {
+/*
+	1. Named return values are declared in the function signature.
+	2. They allow you to return a value without explicitly using the return statement.
+	3. Named return values can be modified within the function, and their final value is returned when the function completes.
+*/
+func sumNamedReturnedValueFunction(a, b int) (result int) {
 	result = a + b
 
 	showResult := func() {
@@ -26,6 +31,13 @@ func sum(a, b int) (result int) {
 	defer showResult()
 	return
 }
+
+/*
+	1. In this case, the function does not have named return values.
+	2. The result is calculated and returned explicitly.
+	3. The defer function modifies the result before it is returned.
+	4. The defer function captures the value of result at the time it is called, not at the time of the return statement.
+*/
 
 func defWithoutNamed(a, b int) int {
 	result := a + b
